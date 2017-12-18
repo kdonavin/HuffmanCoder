84612c45d876c2fd6336d876ecbdca2bf19e3d02
Kirkwood Donavin <kirkwood.donavin@gmail.com>
2017-12-18 12:31:59 -0700

    renamed source directory as required by java packages

--- huffman_code/Driver.java
+++ huffman_code/Driver.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @author Kirkwood Donavin
  */
 public class Driver {
-        /**
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
