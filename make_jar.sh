a8b104d2c22d0630613f4e0fcc0869a82db5a6b9
Kirkwood Donavin <kirkwood.donavin@gmail.com>
2017-12-21 14:53:56 -0800

    minor package name changes

--- make_jar.sh
+++ make_jar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #A script to create a Java archive file
-javac huffman_code/*.java
-jar cfmv huffman_code.jar Manifest.txt huffman_code/*.class #cfmv - creat file, with manifest, verbose
+javac huffman_coder/*.java
+jar cfmv huffman_coder.jar Manifest.txt huffman_coder/*.class #cfmv - creat file, with manifest, verbose
