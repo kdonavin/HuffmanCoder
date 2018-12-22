d3ff600f2e017779b62b888094e4d78a6f15b921
Kirkwood Donavin <kdonavin@womply.com>
2018-12-21 18:44:48 -0700

    ability to pass files through run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #A Bash script to run a built huffman_coder.jar file
 
-java -jar huffman_coder.jar
+if [ $# > 0 ]; then #may also place on next line without ';'
+	java -jar huffman_coder.jar $1
+else
+    java -jar huffman_coder.jar
+fi 
