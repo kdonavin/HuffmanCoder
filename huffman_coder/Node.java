3a894c265038f687c38e548387a2ebb7aea7fa9d
Kirkwood Donavin <agentkirkwood@gmail.com>
2019-03-18 15:39:47 -0600

    Removed stock comment

--- huffman_coder/Node.java
+++ huffman_coder/Node.java
@@ -8,9 +8,7 @@ package huffman_coder;
 import java.lang.Comparable;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
  */
 public class Node implements Comparable<Node> {
     private char key;
