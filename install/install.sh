ecc7591c8bad82aabe582ae0d1adc2e3b284e8fd
Kirkwood Donavin <agentkirkwood@gmail.com>
2025-10-02 00:38:45 -0600

    fixed issue with relative pathing

--- install/install.sh
+++ install/install.sh
@@ -4,12 +4,13 @@
 HUFFDIR="$(cd "$(dirname "$0")/.." && pwd)"
 
 #A script to create a Java archive file
-javac $HUFFDIR/huffman_coder/*.java
-jar cfmv $HUFFDIR/huffman_coder.jar $HUFFDIR/Manifest.txt $HUFFDIR/huffman_coder/*.class #cfmv - create file, with manifest, verbose
+cd "$HUFFDIR"
+javac huffman_coder/*.java
+jar cfmv huffman_coder.jar Manifest.txt huffman_coder/*.class #cfmv -> create file, with manifest, verbose
 
 #Concatenate files
-cat $HUFFDIR/install/stub_exec.sh $HUFFDIR/huffman_coder.jar > $HUFFDIR/huffman_coder.run
-chmod +x $HUFFDIR/huffman_coder.run
+# cat $HUFFDIR/install/stub_exec.sh $HUFFDIR/huffman_coder.jar > $HUFFDIR/huffman_coder.run
+# chmod +x $HUFFDIR/huffman_coder.run
 
 #Clean up
 rm $HUFFDIR/huffman_coder/*.class
